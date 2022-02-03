314062d003fa833b763a9776775da287a7263e26
Ondrej Holecek <oholecek@fortinet.com>
2022-02-03 11:00:29 +0100

    script to convert to plain domain list and the list itself

---
+++ convert/to-plain-domain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+filename="$1"
+if [ -z "$filename" ]; then
+	echo "Usage: $0 <originalFile>" >>/dev/stderr
+	exit 1
+fi
+
+newname="plain-$1"
+
+cat $filename | awk '$1 == "0.0.0.0" { print $2 }' >$newname
+
+echo "Converted $filename to $newname"
